25117c56a8f32f9976e258cac9626166ea87067d
fabmede <fabinhotmedeiros@gmail.com>
2024-03-15 20:44:35 -0300

    Init.

--- backend/business/src/main/java/com/ft/business/security/jwt/JwtProvider.java
+++ backend/business/src/main/java/com/ft/business/security/jwt/JwtProvider.java
@@ -8,7 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.util.Base64;
 import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Component
 public class JwtProvider {
@@ -21,29 +25,38 @@ public class JwtProvider {
     @Value("${jwt.expiration}")
     int expiration;
 
-    public String generateToken(Authentication authentication){
+    public String generateToken(Authentication authentication) {
         UserPrincipal usuarioPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, SignatureAlgorithm.HS512.getValue());
+
         return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .signWith( secretKey)
                 .compact();
     }
 
-    public String getEmailFromToken(String token){
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    public String getEmailFromToken(String token) {
+
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, SignatureAlgorithm.HS512.getValue());
+
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject(); 
     }
 
-    public boolean validateToken(String token){
+    public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            byte[] decodedKey = Base64.getDecoder().decode(secret);
+            SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, SignatureAlgorithm.HS512.getValue());
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
             return true;
-        }catch (MalformedJwtException | UnsupportedJwtException | ExpiredJwtException | IllegalArgumentException | SignatureException e){
+        } catch (MalformedJwtException | UnsupportedJwtException | ExpiredJwtException | IllegalArgumentException e) {
             logger.error("error comprobando el token");
             e.printStackTrace();
         }
         return false;
     }
-
 
 }
